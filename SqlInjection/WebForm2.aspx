10756b3e5b8c9feda418aee2568786b71f454a41
Bhavishya Pothula <Bhavishya_Pothula@epam.com>
2020-02-12 10:57:46 +0530

    Add project files.

---
+++ SqlInjection/WebForm2.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm2.aspx.cs" Inherits="SqlInjection.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Username:<asp:TextBox ID="uname" runat="server"></asp:TextBox>
+        </div>
+        <p>
+            Password:<asp:TextBox ID="password" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="login" runat="server" Text="Login" OnClick="login_Click" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ SqlInjection/WebForm2.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SqlInjection
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void login_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["myconnection"].ConnectionString))
+            {
+                var UN = uname.Text;
+                var PW = password.Text;
+                SqlCommand command;
+                // string qry = "select * from Login where UserName= '' or 1=1 ;
+
+
+
+                command = new SqlCommand("UserLogin", con);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("UserName", UN);
+                command.Parameters.AddWithValue("Password", PW);
+                //
+                // var adpt = new SqlDataAdapter(command, con);
+                con.Open();
+                SqlDataReader sqlDataReader = command.ExecuteReader();
+
+                //Response.Write(count);
+                //  var dt = new DataTable();
+                // adpt.Fill(dt);
+                if (sqlDataReader.HasRows)
+                {
+                    Response.Redirect("WebForm1.aspx");
+                }
+                else
+                {
+                    Response.Write("Invalid Credentials.");
+                }
+            }
+        }
+    }
+}
